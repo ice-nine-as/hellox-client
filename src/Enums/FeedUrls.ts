2f0aa57ab5f9d712f8b8cefe0e77320ec9b5ac87
furkle <furkle@furkleindustries.com>
2018-03-08 21:45:08 -0500

    Fixing typos in FeedUrls.

--- src/Enums/FeedUrls.ts
+++ src/Enums/FeedUrls.ts
@@ -1,16 +1,16 @@
 export enum FeedUrls {
-  NewsFullEn      = 'https://cms.hellox.me/en/news-feed-full.xml',
-  NewsTeasersEn   = 'https://cms.hellox.me/en/news-feed-teaser.xml',
-  NewsTitlesEn    = 'https://cms.hellox.me/en/news-feed-title.xml',  
-  NewsFullNo      = 'https://cms.hellox.me/nb/news-feed-full.xml',
-  NewsTeasersNo   = 'https://cms.hellox.me/nb/news-feed-teaser.xml',
-  NewsTitlesNo    = 'https://cms.hellox.me/nb/news-feed-title.xml',  
-  NewsFullRu      = 'https://cms.hellox.me/ru/news-feed-full-ru.xml',
-  NewsTeasersRu   = 'https://cms.hellox.me/ru/news-feed-teaser.xml',
-  NewsTitlesRu    = 'https://cms.hellox.me/ru/news-feed-title-ru.xml',
-  StoryTemplateEn = 'https://cms.hellox.me/en/story-template-feed.xml',
-  StoryTemplateNo = 'https://cms.hellox.me/nb/story-template-feed.xml',
-  StoryTemplateRu = 'https://cms.hellox.me/ru/story-template-feed.xml',
+  NewsFullEn      = 'https://cms.hellox.me/feeds/en/news-feed-full.xml',
+  NewsTeasersEn   = 'https://cms.hellox.me/feeds/en/news-feed-teaser.xml',
+  NewsTitlesEn    = 'https://cms.hellox.me/feeds/en/news-feed-title.xml',  
+  NewsFullNo      = 'https://cms.hellox.me/feeds/nb/news-feed-full.xml',
+  NewsTeasersNo   = 'https://cms.hellox.me/feeds/nb/news-feed-teaser.xml',
+  NewsTitlesNo    = 'https://cms.hellox.me/feeds/nb/news-feed-title.xml',  
+  NewsFullRu      = 'https://cms.hellox.me/feeds/ru/news-feed-full-ru.xml',
+  NewsTeasersRu   = 'https://cms.hellox.me/feeds/ru/news-feed-teaser.xml',
+  NewsTitlesRu    = 'https://cms.hellox.me/feeds/ru/news-feed-title-ru.xml',
+  StoryTemplateEn = 'https://cms.hellox.me/feeds/en/story-template-feed.xml',
+  StoryTemplateNo = 'https://cms.hellox.me/feeds/nb/story-template-feed.xml',
+  StoryTemplateRu = 'https://cms.hellox.me/feeds/ru/story-template-feed.xml',
 }
 
 export default FeedUrls;
