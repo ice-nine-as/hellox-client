bfe04ac24fec1e19fa151f43c6eb3671f71d19ca
Simen Fjellstad <zaimennor@gmail.com>
2018-04-16 10:40:19 +0200

    adds date and styles podcastitems, comments out lines in storygenerator.

--- src/Components/PodcastItemFull.tsx
+++ src/Components/PodcastItemFull.tsx
@@ -1,87 +1,111 @@
-import {
-  TPodcastItemFullProps,
-} from '../TypeAliases/TPodcastItemFullProps';
+import {TPodcastItemFullProps} from '../TypeAliases/TPodcastItemFullProps';
 
 import * as React from 'react';
+import {getFormattedDate} from './PodcastItemPreview';
 
 // @ts-ignore
 import styles from '../Styles/Components/PodcastItemFull.less';
 const _styles = styles || {};
 
-export class PodcastItemFull extends React.PureComponent<TPodcastItemFullProps> {
-  getPreparedHtml(str: string): { __html: string, } {
-    return { __html: str, };
-  }
+export class PodcastItemFull extends React.PureComponent<
+	TPodcastItemFullProps
+> {
+	getPreparedHtml(str: string): {__html: string} {
+		return {__html: str};
+	}
 
-  render() {
-    const iframe = (() => {
-      if (!this.props.item ||
-        !this.props.item.enclosures ||
-        !this.props.item.enclosures[0] ||
-        !this.props.item.enclosures[0].url) {
-        return 'Embed failed.';
-      }
+	render() {
+		const iframe = (() => {
+			if (
+				!this.props.item ||
+				!this.props.item.enclosures ||
+				!this.props.item.enclosures[0] ||
+				!this.props.item.enclosures[0].url
+			) {
+				return 'Embed failed.';
+			}
 
-      const correctedUrl =
-        /* Add https. */
-        'https' +
-        (this.props.item.enclosures[0].url
-          /* Remove http. */
-          .slice(4));
+			const correctedUrl =
+				/* Add https. */
+				'https' +
+				this.props.item.enclosures[0].url
+					/* Remove http. */
+					.slice(4);
 
-      return (
-        <iframe
-          className={_styles.PodcastIframe}
-          scrolling="no"
-          src={`https://player.blubrry.com/?media_url=${encodeURIComponent(correctedUrl)}`}
-        ></iframe>
-      );
-    })();
+			return (
+				<iframe
+					className={_styles.PodcastIframe}
+					scrolling="no"
+					src={`https://player.blubrry.com/?media_url=${encodeURIComponent(
+						correctedUrl
+					)}`}
+				/>
+			);
+		})();
 
-    const summary = (() => {
-      console.log(this.props.item);
-      if (!this.props.item ||
-        !this.props.item.description) {
-        return 'No description provided.';
-      }
+		const date = (() => {
+			if (!this.props.item || !this.props.item.pubDate) {
+				return 'No date provided.';
+			}
+			return (
+				<p
+					className={_styles.Date}
+					dangerouslySetInnerHTML={this.getPreparedHtml(
+						getFormattedDate(this.props.item.pubDate.toISOString())
+					)}
+				/>
+			);
+		})();
 
-      return (
-        <p
-          className={_styles.Summary}
-          dangerouslySetInnerHTML={this.getPreparedHtml(this.props.item.description)}
-        >
-        </p>
-      );
-    })();
+		const summary = (() => {
+			console.log(this.props.item);
+			if (!this.props.item || !this.props.item.description) {
+				return 'No description provided.';
+			}
 
-    const title = (() => {
-      console.log(this.props.item);
-      if (!this.props.item ||
-        !this.props.item.title) {
-        return 'No Title provided.';
-      }
+			return (
+				<p
+					className={_styles.Summary}
+					dangerouslySetInnerHTML={this.getPreparedHtml(
+						this.props.item.description
+					)}
+				/>
+			);
+		})();
 
-      return (
-        <h1
-          className={_styles.Title}
-          dangerouslySetInnerHTML={this.getPreparedHtml(this.props.item.title)}
-        >
-        </h1>
-      );
-    })();
+		const title = (() => {
+			console.log(this.props.item);
+			if (!this.props.item || !this.props.item.title) {
+				return 'No Title provided.';
+			}
 
-    return (
-      <div className={_styles.PodcastItemFull}>
-        <div
-          className={_styles.ImageWrapper}
-          style={{ backgroundImage: `url('${this.props.item['itunes:image']['#']}')` }}>
-        </div>
-        {title}
-        {iframe}
-        {summary}
-      </div>
-    );
-  }
+			return (
+				<h1
+					className={_styles.Title}
+					dangerouslySetInnerHTML={this.getPreparedHtml(
+						this.props.item.title
+					)}
+				/>
+			);
+		})();
+
+		return (
+			<div className={_styles.PodcastItemFull}>
+				<div
+					className={_styles.ImageWrapper}
+					style={{
+						backgroundImage: `url('${this.props.item[
+							'itunes:image'
+						]['#']}')`,
+					}}
+				/>
+				{title}
+				{date}
+				{iframe}
+				{summary}
+			</div>
+		);
+	}
 }
 
 export default PodcastItemFull;
--- src/Components/PodcastItemPreview.tsx
+++ src/Components/PodcastItemPreview.tsx
@@ -1,89 +1,145 @@
-import {
-  TPodcastItemPreviewProps,
-} from '../TypeAliases/TPodcastItemPreviewProps';
-
+import {TPodcastItemPreviewProps} from '../TypeAliases/TPodcastItemPreviewProps';
 import * as React from 'react';
-
 // @ts-ignore
 import styles from '../Styles/Components/PodcastItemPreview.less';
 const _styles = styles || {};
 
-export class PodcastItemPreview extends React.PureComponent<TPodcastItemPreviewProps> {
-  getPreparedHtml(str: string): { __html: string, } {
-    return { __html: str, }
-  }
+export function getFormattedDate(dateString: string) {
+	const month = [
+		'Jan',
+		'Feb',
+		'Mar',
+		'Apr',
+		'May',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sep',
+		'Oct',
+		'Nov',
+		'Dec',
+	];
+	const myDate = new Date(dateString);
+	let hours = myDate.getHours();
+	const ampm = hours >= 12 ? 'pm' : 'am';
+	hours = hours % 12;
+	hours = hours ? hours : 12;
+	const minutes = myDate.getMinutes();
+	const minuteString = minutes < 10 ? '0' + minutes : minutes;
+	const strTime = hours + ':' + minuteString + ampm;
+	// e.g. "13 Nov 2016 11:00pm";
+	return (
+		myDate.getDate() +
+		' ' +
+		month[myDate.getMonth()] +
+		' ' +
+		myDate.getFullYear() +
+		' ' +
+		strTime
+	);
+}
 
-  render() {
-    const iframe = (() => {
-      if (!this.props.item ||
-        !this.props.item.enclosures ||
-        !this.props.item.enclosures[0] ||
-        !this.props.item.enclosures[0].url) {
-        return 'Embed failed.';
-      }
+export class PodcastItemPreview extends React.PureComponent<
+	TPodcastItemPreviewProps
+> {
+	getPreparedHtml(str: string): {__html: string} {
+		return {__html: str};
+	}
 
-      const correctedUrl =
-        /* Add https. */
-        'https' +
-        (this.props.item.enclosures[0].url
-          /* Remove http. */
-          .slice(4));
+	render() {
+		const iframe = (() => {
+			if (
+				!this.props.item ||
+				!this.props.item.enclosures ||
+				!this.props.item.enclosures[0] ||
+				!this.props.item.enclosures[0].url
+			) {
+				return 'Embed failed.';
+			}
 
-      return (
-        <iframe
-          className={_styles.PodcastIframe}
-          scrolling="no"
-          src={`https://player.blubrry.com/?media_url=${encodeURIComponent(correctedUrl)}`}
-        ></iframe>
-      );
-    })();
+			const correctedUrl =
+				/* Add https. */
+				'https' +
+				this.props.item.enclosures[0].url
+					/* Remove http. */
+					.slice(4);
 
-    const summary = (() => {
-      console.log(this.props.item);
-      if (!this.props.item ||
-        !this.props.item.description) {
-        return 'No description provided.';
-      }
+			return (
+				<iframe
+					className={_styles.PodcastIframe}
+					scrolling="no"
+					src={`https://player.blubrry.com/?media_url=${encodeURIComponent(
+						correctedUrl
+					)}`}
+				/>
+			);
+		})();
 
-      return (
-        <p
-          className={_styles.Summary}
-          dangerouslySetInnerHTML={this.getPreparedHtml(this.props.item.description)}
-        >
-        </p>
-      );
-    })();
+		const summary = (() => {
+			//console.log(this.props.item);
+			if (!this.props.item || !this.props.item.description) {
+				return 'No description provided.';
+			}
 
-    const title = (() => {
-      console.log(this.props.item);
-      if (!this.props.item ||
-        !this.props.item.title) {
-        return 'No Title provided.';
-      }
+			return (
+				<p
+					className={_styles.Summary}
+					dangerouslySetInnerHTML={this.getPreparedHtml(
+						this.props.item.description
+					)}
+				/>
+			);
+		})();
 
-      return (
-        <h3
-          className={_styles.Title}
-          dangerouslySetInnerHTML={this.getPreparedHtml(this.props.item.title)}
-        >
-        </h3>
-      );
-    })();
+		const date = (() => {
+			if (!this.props.item || !this.props.item.pubDate) {
+				return 'No date provided.';
+			}
+			return (
+				<p
+					className={_styles.Date}
+					dangerouslySetInnerHTML={this.getPreparedHtml(
+						getFormattedDate(this.props.item.pubDate.toISOString())
+					)}
+				/>
+			);
+		})();
 
-    return (
-      <div className={_styles.PodcastItemPreview}>
-        <div
-          className={_styles.ImageWrapper}
-          style={{ backgroundImage: `url('${this.props.item['itunes:image']['#']}')` }}>
-        </div>
-        <div className={_styles.TextContent}>
-          {iframe}
-          {title}
-          {summary}
-        </div>
-      </div>
-    );
-  }
+		const title = (() => {
+			//console.log(this.props.item);
+			if (!this.props.item || !this.props.item.title) {
+				return 'No Title provided.';
+			}
+
+			return (
+				<h3
+					className={_styles.Title}
+					dangerouslySetInnerHTML={this.getPreparedHtml(
+						this.props.item.title
+					)}
+				/>
+			);
+		})();
+
+		return (
+			<div className={_styles.PodcastItemPreview}>
+				<div
+					className={_styles.ImageWrapper}
+					style={{
+						backgroundImage: `url('${this.props.item[
+							'itunes:image'
+						]['#']}')`,
+					}}
+				/>
+				<div className={_styles.TextContent}>
+					{iframe}
+					{title}
+					{date}
+					{summary}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default PodcastItemPreview;
