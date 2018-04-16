36052da17a246005cab1f324e4c2c0cb4035e20a
Simen Fjellstad <zaimennor@gmail.com>
2018-04-16 09:52:23 +0200

    Cleans up home and achive pages.

--- src/Components/NewsItemPreview.tsx
+++ src/Components/NewsItemPreview.tsx
@@ -1,6 +1,4 @@
-import {
-  TNewsItemPreviewProps,
-} from '../TypeAliases/TNewsItemPreviewProps';
+import {TNewsItemPreviewProps} from '../TypeAliases/TNewsItemPreviewProps';
 
 import * as React from 'react';
 
@@ -8,19 +6,23 @@ import * as React from 'react';
 import styles from '../Styles/Components/NewsItemPreview.less';
 const _styles = styles || {};
 
-export class NewsItemPreview extends React.PureComponent<TNewsItemPreviewProps> {
-  getPreparedHtml(str: string): { __html: string, } {
-    return { __html: str, }
-  }
+export class NewsItemPreview extends React.PureComponent<
+	TNewsItemPreviewProps
+> {
+	getPreparedHtml(str: string): {__html: string} {
+		return {__html: str};
+	}
 
-  render() {
-    return (
-      <div
-        className={_styles.NewsItemPreview}
-        dangerouslySetInnerHTML={this.getPreparedHtml(this.props.item.description)}
-      ></div>
-    );
-  }
+	render() {
+		return (
+			<div
+				className={_styles.NewsItemPreview}
+				dangerouslySetInnerHTML={this.getPreparedHtml(
+					this.props.item.description
+				)}
+			/>
+		);
+	}
 }
 
 export default NewsItemPreview;
