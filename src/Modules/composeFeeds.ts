68782fe9ce52cdc7f157ad0c24a69760453b0860
furkle <furkle@furkleindustries.com>
2018-04-20 15:15:39 -0400

    Actually fixing issue with feed content sorting this time.

--- src/Modules/composeFeeds.ts
+++ src/Modules/composeFeeds.ts
@@ -17,7 +17,7 @@ export const composeFeeds = (feedOne: IRssFeed | null, feedTwo: IRssFeed | null)
   let duplicates = 0;
   if (feedOne !== null && isRssFeed(feedOne)) {
     composed = feedOne;
-    ids.push(...composed.items.map((item) => item.guid));
+    ids.concat(composed.items.map((item) => item.guid));
     if (feedTwo !== null && isRssFeed(feedTwo)) {
       const newItems = feedTwo.items.map((item) => {
         if (isRssPost(item) && ids.indexOf(item.guid) === -1) {
@@ -36,7 +36,7 @@ export const composeFeeds = (feedOne: IRssFeed | null, feedTwo: IRssFeed | null)
           const dateTwo = new Date(itemTwo.pubDate);
           if (dateOne > dateTwo) {
             return -1;
-          } else if (itemOne.pubDate < itemTwo.pubDate) {
+          } else if (dateOne < dateTwo) {
             return 1;
           } else {
             return 0;
