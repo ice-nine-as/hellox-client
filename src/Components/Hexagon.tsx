9b9eba7da6bfa193ed7c5d7adea4ccbc9b8c9aa6
Simen Fjellstad <zaimennor@gmail.com>
2018-04-23 11:37:55 +0200

    Cleans up hex svg. Stops overflowing horizontally.

--- src/Components/Hexagon.tsx
+++ src/Components/Hexagon.tsx
@@ -10,7 +10,7 @@ const _styles = styles || {};
 const gold = '#dab239';
 const deepPurple = '#343358'
 
-const hexagonImage = `<svg version="1.1" id="dat-hexagon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+const hexagonImage = `<svg version="1.1" id="dat-hexagon" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
 width="240px" height="240px" viewBox="0 0 240 240" enable-background="new 0 0 240 240" xml:space="preserve">
 <polygon fill={color} points="224,179.5 119.5,239.5 15,179.5 15,59.5 119.5,-0.5 224,59.5 "/>
 </svg>`;
@@ -19,7 +19,7 @@ width="240px" height="240px" viewBox="0 0 240 240" enable-background="new 0 0 24
 export class Hexagon extends React.PureComponent<THexagonProps> {
   render() {
     const className = this.props.className ? ` ${this.props.className}` : '';
-    const imageColor = (className == ' active' ? gold : deepPurple)
+    const imageColor = className == ' active' ? gold : deepPurple;
     const formattedSource = hexagonImage.replace('{color}', `"${imageColor}"`)
     const imgSrc = `data:image/svg+xml;utf8,${formattedSource}`
 
