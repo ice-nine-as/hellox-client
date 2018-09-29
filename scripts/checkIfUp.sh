976a77e76548d2c85670aeb96bd2db345f4ef21a
furkle <furkle@furkleindustries.com>
2018-09-29 01:21:53 -0400

    Updating behavior of checkIfUp.sh to copy logs and restart container.

--- scripts/checkIfUp.sh
+++ scripts/checkIfUp.sh
@@ -7,8 +7,9 @@ else
     # Output the State object for debugging.
     echo "Docker container state object:"
     echo "$(/usr/bin/docker inspect --format='{{json .State}}' hellox-client)"
-    # Print the last 50 lines of the log.
-    echo "Docker container logs (last 50 lines):"
-    echo "$(/usr/bin/docker logs hellox-client | tail -50)"
+    # Copy the log.
+    /usr/bin/docker logs hellox-client > ~/docker_error_logs.log
+    # Restart the container
+    /usr/bin/docker start hello-client
     exit 1
 fi
