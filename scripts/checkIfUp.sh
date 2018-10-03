ad2012c1812ab5583e977adb48c4b24ab2f4ce31
furkle <furkle@furkleindustries.com>
2018-10-03 14:52:09 -0400

    Fixing logging portion of monitoring apparatus.

--- scripts/checkIfUp.sh
+++ scripts/checkIfUp.sh
@@ -8,7 +8,8 @@ else
     echo "Docker container state object:"
     echo "$(/usr/bin/docker inspect --format='{{json .State}}' hellox-client)"
     # Copy the log.
-    /usr/bin/docker logs hellox-client > /etc/hellox-client/docker_error_logs.log
+    echo "\nThe hellox-client container failed at $(date '+%Y-%m-%d %H:%M:%S'). Here are the last 100 lines."
+    /usr/bin/docker logs hellox-client --tail 100 >& /etc/hellox-client/docker_error_logs.log
     # Restart the container
     cd /etc/hellox-client/
     gulp dockerUp
